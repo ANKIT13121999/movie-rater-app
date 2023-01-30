68ba5b7d6c74a1bc2f1e2966a4650936ac991001
Ankit Shaw <ankit_shaw@persistent.com>
2023-01-30 19:14:17 +0530

    changed backend url in client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+//https://movie-rater-app-backend.onrender.com/
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://movie-rater-app-backend.onrender.com/api",
 });
 
 export default client;
